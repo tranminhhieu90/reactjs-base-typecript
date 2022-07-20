abae6d8c83d796287c0e53f8d3f3aef0b43d27b0
hieu.dev <hieuminhtm90@gmail.com>
2022-07-20 23:52:11 +0700

    add GlobalStyle, reset css, scss and remove file

---
+++ src/components/GlobalStyles/index.ts
@@ -0,0 +1,11 @@
+import './GlobalStyles.scss';
+
+type Props = {
+    children: JSX.Element;
+};
+
+function GlobalStyles({ children }: Props) {
+    return children;
+}
+
+export default GlobalStyles;
